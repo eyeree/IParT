f249abf89ed6be71cf59ad35e4d8f6477ae5b648
Mike Deem <mike@eyeree.me>
2022-01-06 22:54:07 -0800

    single build script

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,19 +1,29 @@
 const {build} = require("esbuild")
 const esBuildDevServer = require("esbuild-dev-server")
 
-esBuildDevServer.start(
-	build({
-		entryPoints: ["wap/src/index.ts"],
-		outdir: "wap/dist",
-		incremental: true,
-		// and more options ...
-	}),
-	{
-		port:      "8080",
-		watchDir:  "wap/src",
-		index:     "wap/index.html",
-		staticDir: "wap",
-		onBeforeRebuild: {},
-		onAfterRebuild:  {},
-	}
-)
+const mode = process.argv[2] || "build";
+
+const buildResult = build(
+    {
+        entryPoints: ["wap/src/index.ts"],
+        outdir: "wap/dist",
+        incremental: mode == "dev",
+        // and more options ...
+    }
+);
+
+if (mode == "dev") {
+    esBuildDevServer.start(
+        buildResult,
+        {
+            port:      "8080",
+            watchDir:  "wap/src",
+            index:     "wap/index.html",
+            staticDir: "wap",
+            onBeforeRebuild: {},
+            onAfterRebuild:  {},
+        }
+    )
+} else {
+    buildResult.catch(() => process.exit(1));
+}
