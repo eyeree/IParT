a1aa2dbb19b835755639983a4b485255963a3349
Mike Deem <mike@eyeree.me>
2022-01-17 22:08:48 -0800

    converted colors

--- wap/src/Visualizer.ts
+++ wap/src/Visualizer.ts
@@ -58,7 +58,7 @@ const colorShade = (col:string, amt:number) => {
     return `#${rr}${gg}${bb}`
 }
 
-const pallets = [
+const palletsx = [
     {
         name: "purple",
         background: "#400B4F",
@@ -91,6 +91,318 @@ const pallets = [
     }
 ]
 
+const palletsy = [
+    {
+        name: "purple",
+        background: "#400B4F",
+        highlight: "#400B4F",
+        color1: ["#9C9025", "#4F490F"],
+        color2: ["#069C0F", "#074F0B"]
+    },
+    {
+        name: "red",
+        background: "#521D0F",
+        highlight: "#400B4F",
+        color1: ["#2C9E3C", "#13521B"],
+        color2: ["#0D7A9E", "#0B4052"]
+    },
+    {
+        name: "blue",
+        background: "#223C52",
+        highlight: "#400B4F",
+        color1: ["#9E6B51", "#523426"],
+        color2: ["#9E8A31", "#52481D"]
+    },
+    {
+        name: "green",
+        background: colorShade("#0D3614", 20),
+        highlight: "#400B4F",
+        color1: ["#7A2C82", "#321036"],
+        color2: ["#822D12", "#36150A"]
+    },
+    {
+        name: "yellow",
+        background: colorShade("#423B0C", 20),
+        highlight: "pink",
+        color1: ["#28518F", "#0F2442"],
+        color2: ["#640B8F", "#300942"]
+    }
+]
+
+
+const R2_RATIO = 0.3;
+const R3_RATIO = 0.5;
+const R4_RATIO = 0.3;
+const GRID_SIZE = MAX_RADIUS * 2;
+const OUTER_GRID_LEFT = 0;
+const INNER_GRID_LEFT = GRID_SIZE * MAX_RADIUS;
+const NUM_COLOR_STEPS = 20;
+
+// for(const p of pallets) {
+//     p.background = colorShade(p.background, -50);
+//     p.highlight = colorShade(p.background, 20);
+//     const fc1 = chroma.scale([chroma(p.color1[0]).brighten(3), chroma(p.color1[1]).brighten(1)]);
+//     const fc2 = chroma.scale([chroma(p.color2[0]).brighten(3), chroma(p.color2[1]).brighten(1)]);
+//     p.color1 = new Array<string>(NUM_COLOR_STEPS);
+//     p.color2 = new Array<string>(NUM_COLOR_STEPS);
+//     for(let c = 0; c < NUM_COLOR_STEPS; ++c) {
+//         const c1 = fc1(c / (NUM_COLOR_STEPS - 1));
+//         const c2 = fc2(c / (NUM_COLOR_STEPS - 1));
+//         p.color1[c] = c1.hex();
+//         p.color2[c] = c2.hex();
+//     }
+// }
+
+const pallets = [
+    {
+      "name": "purple",
+      "background": "#0e001d",
+      "highlight": "#221431",
+      "color1": [
+        "#ffffb7",
+        "#f8f8b0",
+        "#f1f0a9",
+        "#eae9a3",
+        "#e4e29c",
+        "#ddda96",
+        "#d6d38f",
+        "#cfcc88",
+        "#c8c582",
+        "#c1bd7b",
+        "#bbb675",
+        "#b4af6e",
+        "#ada768",
+        "#a6a061",
+        "#9f995a",
+        "#989154",
+        "#928a4d",
+        "#8b8347",
+        "#847c40",
+        "#7d7439"
+      ],
+      "color2": [
+        "#c2ffa9",
+        "#bbf8a3",
+        "#b4f19d",
+        "#adea97",
+        "#a6e391",
+        "#9fdd8b",
+        "#98d685",
+        "#91cf7f",
+        "#8ac879",
+        "#83c173",
+        "#7cba6d",
+        "#75b367",
+        "#6eac61",
+        "#67a55a",
+        "#609e54",
+        "#59984e",
+        "#529148",
+        "#4b8a42",
+        "#44833c",
+        "#3d7c36"
+      ]
+    },
+    {
+      "name": "red",
+      "background": "#200000",
+      "highlight": "#341414",
+      "color1": [
+        "#cdffcb",
+        "#c6f8c4",
+        "#bff2bd",
+        "#b7ebb6",
+        "#b0e4af",
+        "#a9dda8",
+        "#a2d7a0",
+        "#9ad099",
+        "#93c992",
+        "#8cc28b",
+        "#85bc84",
+        "#7db57d",
+        "#76ae76",
+        "#6fa76f",
+        "#68a168",
+        "#619a61",
+        "#599359",
+        "#528d52",
+        "#4b864b",
+        "#447f44"
+      ],
+      "color2": [
+        "#beffff",
+        "#b7f7f8",
+        "#b1eff1",
+        "#aae8eb",
+        "#a3e0e4",
+        "#9cd8dd",
+        "#96d0d6",
+        "#8fc8d0",
+        "#88c1c9",
+        "#81b9c2",
+        "#7bb1bb",
+        "#74a9b5",
+        "#6da1ae",
+        "#679aa7",
+        "#6092a0",
+        "#598a99",
+        "#528293",
+        "#4c7a8c",
+        "#457385",
+        "#3e6b7e"
+      ]
+    },
+    {
+      "name": "blue",
+      "background": "#000a20",
+      "highlight": "#141e34",
+      "color1": [
+        "#fffddf",
+        "#f8f5d7",
+        "#f2edd0",
+        "#ebe4c8",
+        "#e4dcc0",
+        "#ded3b9",
+        "#d7cbb1",
+        "#d0c3a9",
+        "#c9baa2",
+        "#c3b29a",
+        "#bca993",
+        "#b5a18b",
+        "#af9983",
+        "#a8907c",
+        "#a18874",
+        "#9b7f6d",
+        "#947765",
+        "#8d6f5d",
+        "#876656",
+        "#805e4e"
+      ],
+      "color2": [
+        "#ffffc0",
+        "#f8f8b9",
+        "#f2f0b3",
+        "#ebe9ad",
+        "#e4e2a6",
+        "#dedaa0",
+        "#d7d399",
+        "#d0cc93",
+        "#c9c48c",
+        "#c3bd86",
+        "#bcb580",
+        "#b5ae79",
+        "#afa773",
+        "#a89f6c",
+        "#a19866",
+        "#9b9160",
+        "#948959",
+        "#8d8253",
+        "#877b4c",
+        "#807346"
+      ]
+    },
+    {
+      "name": "green",
+      "background": "#001800",
+      "highlight": "#142c14",
+      "color1": [
+        "#ffbbff",
+        "#f6b5f7",
+        "#eeaeee",
+        "#e5a7e6",
+        "#dda0de",
+        "#d499d5",
+        "#cc92cd",
+        "#c38bc4",
+        "#bb84bc",
+        "#b27db4",
+        "#aa76ab",
+        "#a16fa3",
+        "#99689b",
+        "#906192",
+        "#885b8a",
+        "#7f5482",
+        "#764d79",
+        "#6e4671",
+        "#653f68",
+        "#5d3860"
+      ],
+      "color2": [
+        "#ffb994",
+        "#f7b38f",
+        "#eeac8a",
+        "#e6a584",
+        "#de9f7f",
+        "#d6987a",
+        "#cd9275",
+        "#c58b70",
+        "#bd846b",
+        "#b57e65",
+        "#ac7760",
+        "#a4715b",
+        "#9c6a56",
+        "#946351",
+        "#8b5d4b",
+        "#835646",
+        "#7b5041",
+        "#73493c",
+        "#6a4337",
+        "#623c31"
+      ]
+    },
+    {
+      "name": "yellow",
+      "background": "#241d00",
+      "highlight": "#383114",
+      "color1": [
+        "#c2ddff",
+        "#bbd6f7",
+        "#b4cef0",
+        "#adc6e8",
+        "#a6bfe0",
+        "#9eb7d9",
+        "#97afd1",
+        "#90a8c9",
+        "#89a0c2",
+        "#8298ba",
+        "#7b91b2",
+        "#7489ab",
+        "#6d81a3",
+        "#667a9b",
+        "#5f7294",
+        "#586a8c",
+        "#516384",
+        "#4a5b7d",
+        "#435375",
+        "#3c4b6d"
+      ],
+      "color2": [
+        "#fea3ff",
+        "#f69df7",
+        "#ed97f0",
+        "#e592e8",
+        "#dc8ce0",
+        "#d486d9",
+        "#cb80d1",
+        "#c27ac9",
+        "#ba74c2",
+        "#b16eba",
+        "#a968b2",
+        "#a062ab",
+        "#985ca3",
+        "#8f569b",
+        "#875094",
+        "#7e4a8c",
+        "#754484",
+        "#6d3f7d",
+        "#643975",
+        "#5c336d"
+      ]
+    }
+  ];
+console.log(pallets);
+
+
 export class Visualizer {
 
     private readonly pallet = randa(pallets);
@@ -98,14 +410,6 @@ export class Visualizer {
     private readonly highlight = colorShade(this.background, 20);
     private readonly particlesCanvas = document.createElement("canvas");
     private readonly particlesContext = this.particlesCanvas.getContext('2d')!;
-
-    private readonly R2_RATIO = 0.3;
-    private readonly R3_RATIO = 0.5;
-    private readonly R4_RATIO = 0.3;
-    private readonly GRID_SIZE = MAX_RADIUS * 2;
-    private readonly OUTER_GRID_LEFT = 0;
-    private readonly INNER_GRID_LEFT = this.GRID_SIZE * MAX_RADIUS;
-    private readonly NUM_COLOR_STEPS = 30;
 
     private readonly innerMode:BrightMode;
     private readonly outerMode:BrightMode;
@@ -136,20 +440,21 @@ export class Visualizer {
     }
 
     private prerenderParticles() {
-        this.particlesCanvas.width = (this.GRID_SIZE * (MAX_RADIUS + 1)) * 2;
-        this.particlesCanvas.height = this.GRID_SIZE * this.NUM_COLOR_STEPS;
+        this.particlesCanvas.width = (GRID_SIZE * (MAX_RADIUS + 1)) * 2;
+        this.particlesCanvas.height = GRID_SIZE * NUM_COLOR_STEPS;
 
-        const fc1 = chroma.scale([chroma(this.pallet.color1[0]).brighten(3), chroma(this.pallet.color1[1]).brighten(1)]);
-        const fc2 = chroma.scale([chroma(this.pallet.color2[0]).brighten(3), chroma(this.pallet.color2[1]).brighten(1)]);
+        // const fc1 = chroma.scale([chroma(this.pallet.color1[0]).brighten(3), chroma(this.pallet.color1[1]).brighten(1)]);
+        // const fc2 = chroma.scale([chroma(this.pallet.color2[0]).brighten(3), chroma(this.pallet.color2[1]).brighten(1)]);
 
         for(let r1 = 0; r1 <= MAX_RADIUS; ++r1) {
-            const left = r1 * this.GRID_SIZE;
-            for(let c = 0; c < this.NUM_COLOR_STEPS; ++c) {
-                const c1 = fc1(c / (this.NUM_COLOR_STEPS - 1));
-                const c2 = fc2(c / (this.NUM_COLOR_STEPS - 1));
-                console.log("c", c, "c1", c1.hex(), "c2", c2.hex());
-                this.drawOuterGradient(this.particlesContext, this.GRID_SIZE * c, this.OUTER_GRID_LEFT + left, r1, c1.hex());
-                this.drawInnerGradient(this.particlesContext, this.GRID_SIZE * c, this.INNER_GRID_LEFT + left, r1, c2.hex());
+            const left = r1 * GRID_SIZE;
+            for(let c = 0; c < NUM_COLOR_STEPS; ++c) {
+                // const c1 = fc1(c / (NUM_COLOR_STEPS - 1));
+                // const c2 = fc2(c / (NUM_COLOR_STEPS - 1));
+                const c1 = this.pallet.color1[c];
+                const c2 = this.pallet.color2[c];
+                this.drawOuterGradient(this.particlesContext, GRID_SIZE * c, OUTER_GRID_LEFT + left, r1, c1);
+                this.drawInnerGradient(this.particlesContext, GRID_SIZE * c, INNER_GRID_LEFT + left, r1, c2);
             }
         }
     }
@@ -159,7 +464,7 @@ export class Visualizer {
         const x = left + r1;
         const y = top + r1;
 
-        const r2 = Math.floor(r1*this.R2_RATIO);
+        const r2 = Math.floor(r1*R2_RATIO);
 
         const g = c.createRadialGradient(x, y, r1, x, y, r2)
         g.addColorStop(0, color + "00");
@@ -180,8 +485,8 @@ export class Visualizer {
 
     private drawInnerGradient(c:CanvasRenderingContext2D, top:number, left:number, r1:number, color:string) {
 
-        const r3 = Math.floor(r1*this.R3_RATIO);
-        const r4 = Math.floor(r1*this.R4_RATIO);
+        const r3 = Math.floor(r1*R3_RATIO);
+        const r4 = Math.floor(r1*R4_RATIO);
 
         const x = left + r1;
         const y = top + r1;
@@ -210,10 +515,10 @@ export class Visualizer {
     drawParticle(p:Particle) {
 
         const r = Math.floor(p.radius);
-        const sx1 = this.OUTER_GRID_LEFT + r * this.GRID_SIZE;
-        const sx2 = this.INNER_GRID_LEFT + r * this.GRID_SIZE;
-        const sy1 = this.pickColor(p, this.outerMode) * this.GRID_SIZE;
-        const sy2 = this.pickColor(p, this.innerMode) * this.GRID_SIZE;
+        const sx1 = OUTER_GRID_LEFT + r * GRID_SIZE;
+        const sx2 = INNER_GRID_LEFT + r * GRID_SIZE;
+        const sy1 = this.pickColor(p, this.outerMode) * GRID_SIZE;
+        const sy2 = this.pickColor(p, this.innerMode) * GRID_SIZE;
         const sw = r * 2;
         const sh = sw;
         const dx = p.x - p.radius; // Math.floor(p.x);
@@ -255,7 +560,7 @@ export class Visualizer {
 
     private pickColor(p:Particle, mode:BrightMode):number {
         const f = ColorFactor[mode](p);
-        const i = Math.floor((this.NUM_COLOR_STEPS-1) * f);
+        const i = Math.floor((NUM_COLOR_STEPS-1) * f);
         if(p.trace) {
             console.log("[Visualized] %s - f: %4.4f - i %d - age: %1.4f - speed: %4.4f - dx: %4.4f - dy: %4.4f", BrightMode[mode], f, i, p.age / p.life, p.speed, p.dx, p.dy);
         }
